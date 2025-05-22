a9388edd551d7d9e737e99a951b3045a0a53c082
Victor <oladimejivictor611@gmail.com>
2025-05-22 03:38:36 +0100

    cloudinary working

--- userService/src/main/java/com/skillnest/userservice/service/UserService.java
+++ userService/src/main/java/com/skillnest/userservice/service/UserService.java
@@ -2,12 +2,18 @@ package com.skillnest.userservice.service;
 
 import com.skillnest.userservice.dtos.request.*;
 import com.skillnest.userservice.dtos.response.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface UserService {
     CreatedUserResponse register(CreateUserRequest createUserRequest);
-    void sendEmailValidationOTP(String email);
+
+    String uploadFile(MultipartFile file) throws IOException;
+
+    OTPResponse sendEmailValidationOTP(String email);
     LoginResponse login(LoginRequest loginResponse);
     UpdateUserProfileResponse updateProfile(UpdateUserProfileRequest updateUserProfileRequest);
     ResetPasswordResponse resetPassword(ChangePasswordRequest changePasswordRequest);
-    void sendResetOtp(ResetPasswordRequest resetPasswordRequest);
+    ResetPasswordResponse sendResetOtp(ResetPasswordRequest resetPasswordRequest);
 }

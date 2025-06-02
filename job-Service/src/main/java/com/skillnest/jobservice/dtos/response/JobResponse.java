9d2da8ae6ce98768dd8e8a10a4d48f48f18e426a
Victor <oladimejivictor611@gmail.com>
2025-06-02 22:10:39 +0100

    not done

--- job-Service/src/main/java/com/skillnest/jobservice/dtos/response/JobResponse.java
+++ job-Service/src/main/java/com/skillnest/jobservice/dtos/response/JobResponse.java
@@ -1,27 +1,20 @@
 package com.skillnest.jobservice.dtos.response;
 
 import com.skillnest.jobservice.data.enums.JobStatus;
+import com.skillnest.jobservice.data.model.Job;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Function;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobResponse {
-    private String id;
-    private String title;
-    private String description;
-    private Long employerId;
-    private Long jobSeekerId;
-    private String location;
-    private List<String> requiredSkills;
-    private BigDecimal proposedPayment;
-    private BigDecimal negotiatedPayment;
-    private String jobType;
-    private LocalDateTime deadline;
-    private String contactInfo;
-    private JobStatus status;
-    private LocalDateTime postedDate;
-    private LocalDateTime lastUpdatedDate;
+   private String message;
+   private Job job;
 }

422a96a3c063c70358578d210de783cfc9c2eb1e
Victor <oladimejivictor611@gmail.com>
2025-06-03 22:22:04 +0100

    done

--- job-Service/src/main/java/com/skillnest/jobservice/data/model/Job.java
+++ job-Service/src/main/java/com/skillnest/jobservice/data/model/Job.java
@@ -16,36 +16,21 @@ import java.util.List;
 public class Job {
     @Id
     private String id;
-
     private String title;
-
     private String description;
-
     private String employerId;
-
     private String jobSeekerId;
-
     private String location;
-
+    private WorkImage workImage;
     private List<String> requiredSkills;
-
     private BigDecimal proposedPayment;
-
     private BigDecimal negotiatedPayment;
-
     private String jobType;
-
     private LocalDateTime deadline;
-
     private String contactInfo;
-
     private JobStatus status;
-
     private LocalDateTime postedDate;
-
     private LocalDateTime lastUpdatedDate;
-
     private boolean jobSeekerVerifiedCompletion;
-
     private boolean employerVerifiedCompletion;
 }
